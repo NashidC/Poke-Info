b988ec805f9a647129428edcad8677941de9ce35
david espinal <despinal0425@gmail.com>
2021-02-13 20:47:19 -0500

    push retrieve data from pokemon species in Card-info component

--- poke-info/src/components/home_components/Card-Info.js
+++ poke-info/src/components/home_components/Card-Info.js
@@ -1,10 +1,26 @@
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
 
 
-
-
-function CardInfo(props) { 
+function CardInfo(props) {
+  
+  const [pokemon, setPokemon] = useState('');
 
   console.log(props.pokemonName);
+  let pokemonName = props.pokemonName
+
+  let url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`;
+
+  const fetchPokemon = async () => {
+    const response = await axios.get(url);
+    setPokemon(response.data)
+  }
+
+  useEffect(() => { 
+    fetchPokemon();
+  },[])
+
+  console.log(pokemon)
 
   return (
     <div>
