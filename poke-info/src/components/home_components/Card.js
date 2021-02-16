fdeb214d554e2d415d519e6f6b5bcfd7b1cb2021
david espinal <despinal0425@gmail.com>
2021-02-16 16:41:54 -0500

    added stats to pokemon card

--- poke-info/src/components/home_components/Card.js
+++ poke-info/src/components/home_components/Card.js
@@ -26,6 +26,16 @@ function Card(props) {
     return (
       <div>
         <img src={pokemonInfo.sprites.front_default} />
+        <p>Type</p>
+        <div id="type">
+          <p>{pokemonInfo.types[0].type.name}</p>
+          <p>{pokemonInfo.types[1].type.name}</p>
+        </div>
+        <div id="stat">
+          <p>{pokemonInfo.stats[0].stat.name}: {pokemonInfo.stats[0].base_stat}</p>
+          <p>{pokemonInfo.stats[1].stat.name}: {pokemonInfo.stats[1].base_stat}</p>
+          <p>{pokemonInfo.stats[2].stat.name}: {pokemonInfo.stats[2].base_stat}</p>
+        </div>
       </div>
     )
   } else {
