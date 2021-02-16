1d78f830d55bf9c29c3f8ff08aceb8cd4f969ad0
NashidC <nash.chowdhury@gmail.com>
2021-02-16 13:50:50 -0500

    home page css added

--- poke-info/src/components/home_components/PokemonListTwo.js
+++ poke-info/src/components/home_components/PokemonListTwo.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import NextList from './NextList';
-import Image from "./Image"
 
 function PokemonListTwo(props) {
 
